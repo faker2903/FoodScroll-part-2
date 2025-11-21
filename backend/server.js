ea345ccc36f2a06a2aa5a6c7a263f614119da8fa
faker2903 <ace2903722@gmail.com>
2025-11-21 20:57:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: [ 'https://food-scroll-part-2.vercel.app/register','http://localhost:5173'],
+    origin: [ 'https://food-scroll-part-2.vercel.app','http://localhost:5173'],
     credentials: true
 }));
 app.use(express.json());
